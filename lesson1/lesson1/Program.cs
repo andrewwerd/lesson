dee3f3e220a5093caad0c03da7ad3b2b25d0accb
Andrei Tirsina <andrei.tirsina@amdaris.com>
2020-02-04 15:17:40 +0200

    cercfe

--- lesson1/lesson1/Program.cs
+++ lesson1/lesson1/Program.cs
@@ -9,8 +9,8 @@ namespace lesson1
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Hello World 1");
-
-            Console.WriteLine("Hello world again");
+            Console.WriteLine("abracadabra");
+            Console.WriteLine("abracadabra");
         }
     }
 }
