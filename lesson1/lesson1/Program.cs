37dc27e5f190a9b19d1492be2f21e7107800eda5
Andrei Tirsina <andrei.tirsina@amdaris.com>
2020-02-04 15:39:31 +0200

    dsf

--- lesson1/lesson1/Program.cs
+++ lesson1/lesson1/Program.cs
@@ -6,11 +6,6 @@ namespace lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Hello World 1");
-            
-
         }
     }
 }
