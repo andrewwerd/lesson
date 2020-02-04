f8a21617a8363a2399c52d9190322e9617dcda84
Andrei Tirsina <andrei.tirsina@amdaris.com>
2020-02-04 15:51:25 +0200

    ye

--- lesson1/lesson1/Program.cs
+++ lesson1/lesson1/Program.cs
@@ -9,7 +9,7 @@ namespace lesson1
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Hello World 1");
-            
+            Console.WriteLine("desfcv");
 
         }
         static void method2()
